c0f05242421185756caacbf431a9cd94f2b9e4e0
Benjamin Small <ben20262@aim.com>
2019-11-16 23:41:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.include? {|item| item == cheese_types}
 end
