7e56f0ec3800fa357177c5eeddedb8cef8fb553a
Benjamin Small <ben20262@aim.com>
2019-11-16 23:59:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,8 @@ end
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.select do |item|
-    cheese_types.include?(item)
+    if cheese_types.include?(item)
+      return item
   end
+  nil
 end
