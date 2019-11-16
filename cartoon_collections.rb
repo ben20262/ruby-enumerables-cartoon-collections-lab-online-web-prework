4750db1e36af81749cea83c300e7ddc7781afd63
Benjamin Small <ben20262@aim.com>
2019-11-16 23:34:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planet)
   planet.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.select {|word| word.size?}
 end
 
 def find_the_cheese# code an argument here
